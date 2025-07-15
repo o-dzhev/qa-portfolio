ce552c3abe603be0ae72fd4313c4289be44ca6a2
Olena <admin@Mac.Home>
2025-07-15 19:18:03 +0100

    Cypress project with login tests

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,62 @@
+describe('SauceDemo login with random User', () => {
+    let users;
+    let randomUser;
+
+    before(() => {
+        // Load all users from fixture
+        cy.fixture('users').then((data) => {
+            users = data;
+
+            // Get the keys and pick one randomly
+            const userTypes = Object.keys(users);
+            const randomKey = userTypes[Math.floor(Math.random() * userTypes.length)];
+
+            // Store the randomly selected user
+            randomUser = users[randomKey];
+
+            cy.log(`Using random user: ${randomKey}`);
+        });
+    });
+
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com');
+    });
+
+    it('logs in with a random valid user', () => {
+        cy.get('[data-test="username"]').type(randomUser.username);
+        cy.get('[data-test="password"]').type(randomUser.password);
+        cy.get('[data-test="login-button"]').click();
+        cy.url().should('include', '/inventory.html');
+
+    });
+
+    it('log in fails with invalid username', () => {
+        cy.get('[data-test="username"]').type('invalid');
+        cy.get('[data-test="password"]').type(randomUser.password);
+        cy.get('[data-test="login-button"]').click();
+        cy.contains('Epic sadface: Username and password do not match any user in this service').should('be.visible');
+
+    });
+
+    it('log in fails with invalid password', () => {
+        cy.get('[data-test="username"]').type(randomUser.username);
+        cy.get('[data-test="password"]').type('000000');
+        cy.get('[data-test="login-button"]').click();
+        cy.contains('Epic sadface: Username and password do not match any user in this service').should('be.visible');
+
+    });
+
+    it('log in fails with invalid username & password', () => {
+        cy.get('[data-test="username"]').type('invalid');
+        cy.get('[data-test="password"]').type('000000');
+        cy.get('[data-test="login-button"]').click();
+        cy.contains('Epic sadface: Username and password do not match any user in this service').should('be.visible');
+
+    });
+
+    it('log in fails with empty fields', () => {
+        cy.get('[data-test="login-button"]').click();
+        cy.contains('Epic sadface: Username is required').should('be.visible');
+
+    });
+});
